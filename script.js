ba7434c213233ae2275b0f82f7bcbcc683b47f05
alyssadrafts <natasyahebat@gmail.com>
2024-05-09 21:32:55 +0800

    updated

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+  
+  let prevScrollpos = window.pageYOffset;
+    let navBar = document.getElementById("desktop-nav");
+
+    window.onscroll = function() {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            // Scrolling up, reveal the navbar
+            navBar.style.top = "0";
+        } else {
+            // Scrolling down, hide the navbar
+            navBar.style.top = "-17vh";
+        }
+        prevScrollpos = currentScrollPos;
+    }
